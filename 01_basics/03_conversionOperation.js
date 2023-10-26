790e8a6d8abc31982be7b320ae451c089d0fd9e5
Shubham kumar <82118826+its-shubham-kalwar@users.noreply.github.com>
2023-10-26 19:05:06 +0000

    container config push

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,24 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ************************* 0perations *******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Shubham"
+
+
+let str3 = str1 + str2
+console.log
