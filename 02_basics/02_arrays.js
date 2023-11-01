5f4f02bed57b7e99e3ed81b4ed5ffeb986e325f7
Shubham kumar <82118826+its-shubham-kalwar@users.noreply.github.com>
2023-11-01 06:24:38 +0000

    container config push

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,48 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+
+// marvel_heros.concat(dc_heros)
+// console.log(marvel_heros); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros); // [
+//     'thor',
+//     'Ironman',
+//     'spiderman',
+//     [ 'superman', 'flash', 'batman' ],
+//     'superman',
+//     'flash',
+//     'batman'
+//   ]
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const another_array = [1,2,3, [4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array); // [
+//     1, 2, 3, 4, 5,
+//     6, 7, 6, 7, 4,
+//     5
+//   ]
+
+// 
+
+// console.log(Array.isArray("Shubham")); // false
+// console.log(Array.from("Kumar")); // [ 'K', 'u', 'm', 'a', 'r' ]
+
+
+// console.log(Array.from({name : "Shubham"})); // []
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+// console.log(Array.of(score1, score2, score3)); // [ 100, 200, 300 ]
+ 
+
