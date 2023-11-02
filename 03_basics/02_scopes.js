5afe9060ddd0455eaf3cf7d9b56a1e7df2d81e3d
Shubham kumar <82118826+its-shubham-kalwar@users.noreply.github.com>
2023-11-02 07:14:22 +0000

    container config push

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,21 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a); // 10
+// console.log(b); // 20
+// console.log(c); // 30
+
+// var c = 300
+let b = 300
+
+if(true) {
+    let a = 10
+    const b = 20 
+    // var c = 30
+}
+
+// console.log(a); 
+console.log(b); 
+// console.log(c); 
+
