0f4b27c27fb821434d2150a138328d66bbfaa3c4
Shubham kumar <82118826+its-shubham-kalwar@users.noreply.github.com>
2023-11-02 07:03:04 +0000

    container config push

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -50,12 +50,53 @@ const result = addTwoNumbers(3,5)
 // undefined
 
 
-function loginUserMessage(username = "Shubham"){
-    if(username === undefined){
-        console.log("Please enter a username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username = "Shubham"){
+//     if(username === undefined){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 // console.log(loginUserMessage()); // Shubham just logged in 
-console.log(loginUserMessage("kalwar")); // kalwar just logged in
+// console.log(loginUserMessage("kalwar")); // kalwar just logged in
+
+
+// function calculatePrice(...num1){ // rest operator
+//     return num1
+// }
+
+// console.log(calculatePrice(200, 400, 500, 2000)); [200, 400, 500, 2000]
+
+
+function calculatePrice(val1, val2, ...num1){ // rest operator
+    return num1
+}
+
+// console.log(calculatePrice(200, 400, 500, 2000)); [500, 2000]
+
+const user = {
+    username : "Shubham",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user) // Username is Shubham and price is 199
+
+
+handleObject({
+    username : "Sam",
+    price : 399
+}) // Username is Sam and price is 399
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray)); // 400
+
+console.log(returnSecondValue([200, 400, 100, 600])); // 400
